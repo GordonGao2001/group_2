05547aff2f30680605eea56814c741536552bfcd
Gordon <d.gao@student.vu.nl>
2024-11-13 21:41:32 +0100

    setup env

---
+++ set_up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install -r requirements.txt
+echo -e "   *\n  ***\n *****\n*******"
+echo 'if you want to use other LLM please follow instructions on https://www.llama.com/llama-downloads/'
+echo -e "   *\n  ***\n *****\n*******"
+echo 'or use this link https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/blob/main/llama-2-7b-chat.Q4_K_M.gguf'
