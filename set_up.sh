5140f5cccd8d8c9a5af80df638ae3b97a1122869
Gordon <d.gao@student.vu.nl>
2024-11-13 21:47:00 +0100

    added more to readme

--- set_up.sh
+++ set_up.sh
@@ -6,3 +6,5 @@ echo -e "   *\n  ***\n *****\n*******"
 echo 'if you want to use other LLM please follow instructions on https://www.llama.com/llama-downloads/'
 echo -e "   *\n  ***\n *****\n*******"
 echo 'or use this link https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/blob/main/llama-2-7b-chat.Q4_K_M.gguf'
+echo -e "   *\n  ***\n *****\n*******"
+echo 'remember to put the model into ./models!!!!'
